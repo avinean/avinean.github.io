60c9d78bb4df5ab1d370e551da7ebae2f6e135f7
Vladyslav Lubeznyy <avinean@gmail.com>
2017-12-15 18:47:54 +0200

    hover effects for icons

--- proj/blog/core/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-    define("SERVERNAME", 'databases-auth.000webhost.com');
-    define("DBNAME", 'id3784881_blogdb');
-    define("DBUSERNAME", 'id3784881_avinean');
-    define("DBPASS", 'Coba1953');
-?>
